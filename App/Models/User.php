5bc615dd394e5b36ccfa71277007efde60fafc77
Alex <alexander.stephen.wall@gmail.com>
2019-05-06 00:22:01 +0000

    Fixing static referencing of self in models.

--- App/Models/User.php
+++ App/Models/User.php
@@ -25,7 +25,7 @@ class User extends DatabaseEncapsulator
 
 	public static function createInactiveUser($username, $email, $passwordHash, $identifier)
 	{
-		return $self->createModelWithEntries([
+		return self::createModelWithEntries([
 			'username' => $username,
 			'email' => $email,
 			'password' => $passwordHash,
