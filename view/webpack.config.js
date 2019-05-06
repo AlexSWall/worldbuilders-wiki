904260c9a450e9b5ccd46b7de728abfe30aee673
Alex <alexander.stephen.wall@gmail.com>
2019-05-06 02:29:27 +0000

    Added hot-reloading/watching for the front-end.

--- view/webpack.config.js
+++ view/webpack.config.js
@@ -1,38 +1,42 @@
 const path = require('path');
+const webpack = require('webpack');
+
+new webpack.HotModuleReplacementPlugin();
 
 module.exports = {
-  entry: {
-    app: ['@babel/polyfill', 
-      './src/index.js']
-  },
-  output: {
-    path: path.resolve(__dirname, '..', 'public', 'resources', 'wiki', 'js'),
-    filename: 'wiki.bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|tsx|ts)$/,
-        resolve: { 
-          alias: {
-            Components: path.resolve(__dirname, 'src/components/'),
-            Scripts: path.resolve(__dirname, 'src/scripts/'),
-            Utilities: path.resolve(__dirname, 'src/utilities/')
-          },
-          extensions: [".js", ".jsx"]
-        }, 
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      }
-    ]
-  },
-  mode: 'production',
-  externals: {
-    myApp: 'myApp'
-  }
+	entry: {
+		app: ['@babel/polyfill', 
+			'./src/index.js']
+	},
+	output: {
+		path: path.resolve(__dirname, '..', 'public', 'resources', 'wiki', 'js'),
+		filename: 'wiki.bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx|tsx|ts)$/,
+				resolve: { 
+					alias: {
+						Components: path.resolve(__dirname, 'src/components/'),
+						Scripts: path.resolve(__dirname, 'src/scripts/'),
+						Utilities: path.resolve(__dirname, 'src/utilities/')
+					},
+					extensions: [".js", ".jsx"]
+				}, 
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react'],
+						plugins: ['react-hot-loader/babel']
+					}
+				}
+			}
+		]
+	},
+	mode: 'development',
+	externals: {
+		myApp: 'myApp'
+	}
 };
