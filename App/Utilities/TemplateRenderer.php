c416fecf4b84acf91340464360fd5b5e09b3b7b5
Alex <alexander.stephen.wall@gmail.com>
2019-05-14 00:57:46 +0000

    Added templating for images, lists, paragraphs, and bold+italics.

--- App/Utilities/TemplateRenderer.php
+++ App/Utilities/TemplateRenderer.php
@@ -16,8 +16,17 @@ class TemplateRenderer
 	public static function renderTemplate($pageName, $templateContent)
 	{
 		$workingContent = htmlspecialchars($templateContent, ENT_QUOTES, 'UTF-8');
+		$workingContent = str_replace("\r", '', $workingContent);
 
 		$patternAndReplacementPairs = [
+
+			/* == Images == */
+			/* [[Image:"url"|width,height]]  ->  <img src="url" width="width" height="height"> */
+			'/\[\[Image:&quot;([^\]\[\|]+)&quot; *\| *(\d+), *(\d+)\]\]/' => function($matches)
+				{
+					[$str, $location, $width, $height] = $matches;
+					return "<img src=\"/resources/images/{$location}\" width=\"{$width}\" height=\"{$height}\">";
+				},
 
 			/* == Links == */
 			/* [[text]]  ->  <a href="/#text_with_underscores\">text</a> */
@@ -49,6 +58,44 @@ class TemplateRenderer
 					$heading = $matches[2];
 
 					return "<h{$secNum}>{$heading}</h{$secNum}>";
+				},
+
+			/* == Lists == */
+			'/(((\n|^)[^-\n][^\n]*|^)\s*)(-[^\n]*(\n|$)([ \t]*((\n|$)|(-[^\n]*(\n|$))))*)(\s*($|[^-]))/' => function($matches)
+				{
+					return "{$matches[1]}<ul>\n{$matches[4]}\n</ul>{$matches[11]}";
+				},
+
+			'/\n[ \t]*-[ \t]*([^\n]*)/' => function($matches)
+				{
+					return "\n<li>{$matches[1]}</li>";
+				},
+
+			/* == Paragraphs == */
+			/* The beginning or >=2 new lines until the end or two new lines, with anything between, matched lazily. */
+			'/(^\n*|\n\n+)([^<>\[\]=][\S\s]+?[^<>\[\]=])(?=(\n*$|\n\n))/' => function($matches)
+				{
+					[$str, $start, $middle, $end] = $matches;
+					return "{$start}<p>{$middle}</p>{$end}";
+				},
+
+			/* == Bold & Italics == */
+			/* ''text''  ->  <i>text</i> */
+			"/[^']''([^']|[^'][\S\s]*?[^'])''[^']/" => function($matches)
+				{
+					return "<i>{$matches[1]}</i>";
+				},
+
+			/* '''text'''  ->  <b>text</b> */
+			"/[^']'''([^']|[^'][\S\s]*?[^'])'''[^']/" => function($matches)
+				{
+					return "<b>{$matches[1]}</b>";
+				},
+
+			/* '''''text'''''  ->  <b><i>text</i></b> */
+			"/[^']'''''([^']|[^'][\S\s]*?[^'])'''''[^']/" => function($matches)
+				{
+					return "<b><i>{$matches[1]}</i></b>";
 				}
 		];
 
