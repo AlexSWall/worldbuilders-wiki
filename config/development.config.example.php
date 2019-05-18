3525bec46207de6a81acbb190c692c6241f3a443
Alex <alexander.stephen.wall@gmail.com>
2019-05-18 20:09:26 +0000

    Fixing minor initial setup imperfections.

--- config/development.config.example.php
+++ config/development.config.example.php
@@ -1,25 +1,72 @@
 <?php
+
+/* Example basic variables for config with gmail server. */
+$baseUrl = ''
+$db_name = ''
+$db_username = ''
+$db_password = ''
+$email = ''
+$email_password = ''
+$name = ''
+
 return [
 	'displayErrorDetails' => true,
 	'addContentLengthHeader' => false,
 	'app' => [
-		'url' => 'http://localhost',
+		'url' => $baseUrl,
 		'hash' => [
-			'algo' => PASSWORD_BCRYPT,
-			'cost' => 10
-		]
+			'password_hash_algorithm' => PASSWORD_BCRYPT,
+			'cost' => 10,
+			'standard_hash_algorithm' => 'sha256'
+		],
+		'logging' => true
 	],
-	'logger' => [
-		'name' => 'channel name',
-		'level' => Monolog\Logger::DEBUG,
-		'path' => BASE_PATH . '/relative/path.log'
+	'loggers' => [
+		/* Loggers: setup, general, network, database, security */
+		/* Could be expanded to include: business, statistics, suspicious-activity, ... */
+		\App\Logging\LoggerRegistry::SETUP_LOGGER => [
+			'logger_name' => 'setup_logger',
+			'channel_name' => \App\Logging\LoggerRegistry::SETUP_LOGGER,
+			'level' => Monolog\Logger::DEBUG,
+			'path' => BASE_PATH . '/logs/website.log'
+		],
+		\App\Logging\LoggerRegistry::GENERAL_LOGGER => [  /* The default logger */
+			'logger_name' => 'gen_logger',
+			'channel_name' => \App\Logging\LoggerRegistry::GENERAL_LOGGER,
+			'level' => Monolog\Logger::DEBUG,
+			'path' => BASE_PATH . '/logs/website.log'
+		],
+		\App\Logging\LoggerRegistry::NETWORK_LOGGER => [
+			'logger_name' => 'net_logger',
+			'channel_name' => \App\Logging\LoggerRegistry::NETWORK_LOGGER,
+			'level' => Monolog\Logger::DEBUG,
+			'path' => BASE_PATH . '/logs/website.log'
+		],
+		\App\Logging\LoggerRegistry::DATABASE_LOGGER => [
+			'logger_name' => 'db_logger',
+			'channel_name' => \App\Logging\LoggerRegistry::DATABASE_LOGGER,
+			'level' => Monolog\Logger::DEBUG,
+			'path' => BASE_PATH . '/logs/website.log'
+		],
+		\App\Logging\LoggerRegistry::SECURITY_LOGGER => [
+			'logger_name' => 'sec_logger',
+			'channel_name' => \App\Logging\LoggerRegistry::SECURITY_LOGGER,
+			'level' => Monolog\Logger::DEBUG,
+			'path' => BASE_PATH . '/logs/website.log'
+		],
+		\App\Logging\LoggerRegistry::DEBUG_LOGGER => [
+			'logger_name' => 'debug_logger',
+			'channel_name' => \App\Logging\LoggerRegistry::DEBUG_LOGGER,
+			'level' => Monolog\Logger::DEBUG,
+			'path' => BASE_PATH . '/logs/website.log'
+		]
 	],
 	'db' => [
 		'driver' => 'mysql',
 		'host' => '127.0.0.1',
-		'database' => 'database',
-		'username' => 'username',
-		'password' => 'password',
+		'database' => $db_name,
+		'username' => $db_username,
+		'password' => $db_password,
 		'charset' => 'utf8',
 		'collation' => 'utf8_unicode_ci',
 		'prefix' => ''
@@ -33,11 +80,11 @@ return [
 		'smtp_auth' => true,
 		'smtp_secure' => 'tls',
 		'port' => 587,
-		'username' => 'email address',
-		'password' => 'password',
+		'username' => $email,
+		'password' => $email_password,
 		'html' => true,
-		'from_email' => 'email address',
-		'from_name' => 'name'
+		'from_email' => $email,
+		'from_name' => $name
 	],
 	'twig' => [
 		'debug' => true
