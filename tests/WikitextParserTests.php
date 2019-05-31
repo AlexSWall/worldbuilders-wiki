10822974b04d395c2eba5b43ca6508a642156a47
Alex <alexander.stephen.wall@gmail.com>
2019-05-31 20:04:37 +0000

    Fixed complex grammar test.

--- tests/WikitextParserTests.php
+++ tests/WikitextParserTests.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types=1);
+<?php declare( strict_types = 1 );
 
 require dirname(__DIR__) . '/bootstrap/app.php';
 
@@ -26,9 +26,17 @@ final class WikitextParserTests extends TestCase
 
 	private function wikitextConversionTester($wikitextToConvert, $expected): void
 	{
-		$this->assertSame( $expected,
-			( new WikitextConverter() )->convertWikitextToHTML($wikitextToConvert)
-		);
+		$this->assertSame( $expected, ( new WikitextConverter() )->convertWikitextToHTML($wikitextToConvert) );
+	}
+
+	public function testBasicHeaderConversion(): void
+	{
+		$this->wikitextConversionTester('==Header==', '<h2>Header</h2>');
+	}
+
+	public function testBasicHeaderConversion2(): void
+	{
+		$this->wikitextConversionTester('==  Header ===', '<h2>Header</h2>');
 	}
 
 	public function testBasicParagraphConversion(): void
@@ -41,6 +49,22 @@ final class WikitextParserTests extends TestCase
 		$this->wikitextConversionTester(
 			"P1 Line1\nP1 Line2\n\nP2 Line1\nP2 Line2\n\nP3 Line3",
 			"<p>P1 Line1\nP1 Line2</p>\n\n<p>P2 Line1\nP2 Line2</p>\n\n<p>P3 Line3</p>"
+		);
+	}
+
+	public function testHeaderBetweenParagraphs(): void
+	{
+		$this->wikitextConversionTester(
+			"First paragraph.\n==Heading==\nSecond paragraph.",
+			"<p>First paragraph.</p>\n<h2>Heading</h2>\n<p>Second paragraph.</p>"
+		);
+	}
+
+	public function testComplexWikitextConversion(): void
+	{
+		$this->wikitextConversionTester(
+			" Intro  \n== Heading ===\nP2L1\nP2L2\n== Heading  2==\n\n\n '''P''3L''1''' \n ====Sub Heading===\nend",
+			"<p>Intro</p>\n<h2>Heading</h2>\n<p>P2L1\nP2L2</p>\n<h2>Heading  2</h2>\n\n\n<p><b>P<i>3L</i>1</b></p>\n<h3>Sub Heading</h3>\n<p>end</p>"
 		);
 	}
 }
