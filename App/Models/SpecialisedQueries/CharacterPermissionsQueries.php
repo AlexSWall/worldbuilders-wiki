c7a4f65fbd62ed03120d925b06e7c94e63f9d9e4
Alex <alexander.stephen.wall@gmail.com>
2019-06-09 20:16:59 +0000

    Added character permission methods to Character.php, with specialised queries for efficiency.

--- App/Models/SpecialisedQueries/CharacterPermissionsQueries.php
+++ App/Models/SpecialisedQueries/CharacterPermissionsQueries.php
@@ -8,25 +8,6 @@ use App\Utilities\ArrayBasedSet;
 
 class CharacterPermissionsQueries
 {
-	private static function getCharacterPermissionRelationsTable()
-	{
-		return ;
-	}
-
-	/**
-	 *
-	 *
-	 */
-	public static function removeCharacterPermissions($characterId, $permissions)
-	{
-		if ( count($permissions) == 0 )
-			return;
-
-		DB::table('CharacterPermissionRelations')
-				->where('characterId', $characterId)
-				->whereIn($permissions)
-	}
-
 	/**
 	 * SELECT PermissionName
 	 *     FROM WikiPermissions
@@ -34,15 +15,65 @@ class CharacterPermissionsQueries
 	 *         ON WikiPermissions.PermissionId = CharacterPermissionRelations.PermissionId
 	 *     WHERE CharacterPermissionRelations.CharacterId = {$CharacterId};
 	 */
-	public static function runCharacterIdToPermissionNamesQuery($characterId)
+	public static function getCharacterPermissions($characterId): object
 	{
-		$permissionsArray = DB::table('WikiPermissions')
+		$permissionNameStdClassArray = DB::table('WikiPermissions')
 				->select('PermissionName')
 				->join('CharacterPermissionRelations', 'WikiPermissions.PermissionId', '=', 'CharacterPermissionRelations.PermissionId')
 				->where('CharacterPermissionRelations.CharacterId', $characterId)
-				->all();
+				->get()->all();
+
+		$permissionsArray = array();
+		foreach( $permissionNameStdClassArray as $permissionNameStdClass )
+			$permissionsArray[] = $permissionNameStdClass->PermissionName;
+
 		$permissionsSet = new ArrayBasedSet($permissionsArray);
 		return $permissionsSet;
+	}
+
+	/**
+	 * Insert a collection of permissions, by name, to a character, by Id.
+	 */
+	public static function addCharacterPermissions($characterId, $permissions): void
+	{
+		if ( count($permissions) == 0 )
+			return;
+
+		if ( is_a($permissions, 'App\Utilities\SetInterface') )
+			$permissions = $permissions->values();
+
+		$permissionIdStdClassArray = DB::table('WikiPermissions')
+				->select('PermissionId')
+				->whereIn('PermissionName', $permissions)
+				->get()->all();
+
+		$insertion = array();
+		foreach ( $permissionIdStdClassArray as $permissionIdStdClass )
+			$insertion[] = ['CharacterId' => $characterId, 'PermissionId' => $permissionIdStdClass->PermissionId ];
+
+		DB::table('CharacterPermissionRelations')->insert($insertion);
+	}
+
+	/**
+	 * DELETE r FROM CharacterPermissionRelations r
+	 *     INNER JOIN WikiPermissions p
+	 *         ON p.`PermissionId` = r.`PermissionId`
+	 *     WHERE r.`CharacterId` = {$CharacterId}
+	 *     AND p.`PermissionName` IN ('permission1', 'permission2',...);
+	 */
+	public static function removeCharacterPermissions($characterId, $permissions): void
+	{
+		if ( count($permissions) == 0 )
+			return;
+
+		if ( is_a($permissions, 'App\Utilities\SetInterface') )
+			$permissions = $permissions->values();
+
+		DB::table('CharacterPermissionRelations')
+				->join('WikiPermissions', 'CharacterPermissionRelations.PermissionId', '=', 'WikiPermissions.PermissionId')
+				->where('CharacterPermissionRelations.CharacterId', $characterId)
+				->whereIn('WikiPermissions.PermissionName', $permissions)
+				->delete();
 	}
 }
 
