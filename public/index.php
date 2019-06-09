beb0b7c3818ad2ca267fc5f4b7f2f0ea3dfd4308
Alex <alexander.stephen.wall@gmail.com>
2019-06-09 20:13:24 +0000

    Improved bootstrapping to be more generalisable and added testing bootstrapper.

--- public/index.php
+++ public/index.php
@@ -1,7 +1,9 @@
 <?php
 
-$app = null;
+define('BASE_PATH', dirname(__DIR__));
 
-require dirname(__DIR__) . '/bootstrap/app.php';
+$config = require BASE_PATH . '/config/' . trim(file_get_contents(BASE_PATH . '/mode.php')) . '.config.php';
+
+$app = require BASE_PATH . '/bootstrap/app.php';
 
 $app->run();
