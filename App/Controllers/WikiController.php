c9d0dafc6bb53c006b277298b6844ed6ff9aab0b
Alex <alexander.stephen.wall@gmail.com>
2019-06-10 19:35:35 +0000

    Added conditional rendering of wikipages dependent on section permissions.

--- App/Controllers/WikiController.php
+++ App/Controllers/WikiController.php
@@ -3,14 +3,14 @@
 namespace App\Controllers;
 
 use App\Models\Webpage;
-use App\Helpers\FrontEndDataUtils;
+use App\Helpers\FrontEndDataUtilities;
 
 class WikiController extends Controller
 {
 	/* Request with URL of the form  '<BaseURL>/#{pageName}'  */
 	public function serveWikiApp($request, $response)
 	{
-		return FrontEndDataUtils::getEntryPointResponse( $this->view, $response, 'wiki' );
+		return FrontEndDataUtilities::getEntryPointResponse( $this->view, $response, 'wiki' );
 	}
 
 	/* Request with URL of the form  '<BaseURL>/w/{pageName}'  */
@@ -18,10 +18,16 @@ class WikiController extends Controller
 	{
 		$pageName = $args['pageName'];
 
+		$character = $this->auth->getCharacter();
+		if ( $character )
+			$viewingPermissions = $character->getPermissions();
+		else
+			$viewingPermissions = null;
+
 		if ( substr($pageName, 0, strlen('Special:')) === 'Special:' )
 			$data = $this->getSpecialContentData($pageName);
 		else 
-			$data = self::getDatabaseWebpageData($pageName);
+			$data = self::getDatabaseWebpageData($pageName, $viewingPermissions);
 
 		$response = $response->withJSON([
 			'webpage' => $data
@@ -30,20 +36,20 @@ class WikiController extends Controller
 		return $response;
 	}
 
-	private static function getDatabaseWebpageData($pageName)
+	private static function getDatabaseWebpageData($pageName, $viewingPermissions)
 	{
-		$webpage = Webpage::retrieveWebpageByName($pageName);
+		$webpage = Webpage::retrieveWebpageByUrlPath($pageName);
 
 		if ( is_null($webpage) )
-			$webpage = Webpage::retrieveWebpageByName('Page_Not_Found');
+			$webpage = Webpage::retrieveWebpageByUrlPath('Page_Not_Found');
 
-		$webpage->renderWebpageTemplateToHTML();
+		$webpage->renderWikiTextToHtmlBlocks();
 
-		return [
-			'name' => $webpage->getWebpageName(),
-			'title' => $webpage->getWebpageTitle(),
-			'HTML' => $webpage->getWebpageHTML()
-		];
+		return FrontEndDataUtilities::constructEndpointDataArray(
+			$webpage->getUrlPath(),
+			$webpage->getTitle(),
+			$webpage->getHtmlForPermissionsExpression($viewingPermissions)
+		);
 	}
 
 	private function getSpecialContentData($pageName)
@@ -57,7 +63,7 @@ class WikiController extends Controller
 			case 'Delete_Wiki_Page':
 				return $this->WikiPageController->getDeleteWebpageData();
 			case 'Template_Formatting':
-				return FrontEndDataUtils::getWebpageDataFor($pageName, 'Meta: Template Formatting', 
+				return FrontEndDataUtilities::getWebpageDataFor($pageName, 'Meta: Template Formatting', 
 					$this->view, 'templateformatting');
 			default:
 				return $this->getDatabaseWebpageData('Page_Not_Found');
