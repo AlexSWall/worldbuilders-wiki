bcb349b5e730ef0138a5074211ddd9685ab8828e
Alex <alexander.stephen.wall@gmail.com>
2019-06-11 19:17:15 +0000

    Improved footer asthetically and functionally.

--- View/src/Components/Footer.jsx
+++ View/src/Components/Footer.jsx
@@ -8,11 +8,25 @@ class Footer extends Component
 		return (
 			<div id="footerWrapper">
 				<footer>
-					<ul>
-						<FooterItem href="/#About" text="About" />
-						<FooterItem href="/#Contact" text="Contact" />
-						<FooterItem href="/#Bugs" text="Known Bugs" />
-					</ul>
+					<div id="footerContent">
+						<div id="leftFooterContent">
+							<div id="leftFooterLine">
+								<div id="leftFooterLineItem">
+									Alex's World's D<small>&</small>D Wiki
+								</div>
+								<div id="leftFooterLineItem">
+									Dungeons & Dragons is property of Wizards of the Coast LLC in the U.S.A. and other countries. ©2019 Wizards.
+								</div>
+							</div>
+						</div>
+						<div id="rightFooterContent">
+							<div id="rightFooterLine">
+								<FooterItem href="/#About" text="About" />
+								<FooterItem href="/#Contact" text="Contact" />
+								<FooterItem href="/#Bugs" text="Known Bugs" />
+							</div>
+						</div>
+					</div>
 				</footer>
 			</div>
 		);
