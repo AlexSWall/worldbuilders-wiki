8722c20a03b7ee986936d394afe286c0802a2118
Alex <alexander.stephen.wall@gmail.com>
2019-06-14 18:53:51 +0000

    Removed dependency on AwesomeFont for search icon by replacing with SVG path.

--- View/src/Components/NavigationBar/NavBarSearchBar.jsx
+++ View/src/Components/NavigationBar/NavBarSearchBar.jsx
@@ -9,7 +9,9 @@ class NavBarSearchBar extends Component
 					<div className="searchbar">
 						<input type="text" className="searchbar-input" placeholder="What are you looking for?" />
 						<button type="submit" className="searchbar-button">
-							<i className="fa fa-search"></i>
+							<svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" focusable="false" style={ {'pointerEvents': 'none', display: 'block', width: '100%', height: '100%'} }>
+								<path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+							</svg>
 						</button>
 					</div>
 				</div>
@@ -19,4 +21,3 @@ class NavBarSearchBar extends Component
 }
 
 export default NavBarSearchBar;
-
