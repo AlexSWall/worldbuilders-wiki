597a2a50e092a32a936f28cc6275dd364c1c11e2
Alex <alexander.stephen.wall@gmail.com>
2019-07-15 13:23:09 +0000

    Renamed NavBarButton to NavBarLink.

--- View/src/Components/NavigationBar/NavBarContent.jsx
+++ View/src/Components/NavigationBar/NavBarContent.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import NavBarList         from './NavBarList';
 import NavBarDropdown     from './NavBarDropdown';
 import NavBarDropdownItem from './NavBarDropdownItem';
-import NavBarButton       from './NavBarButton';
+import NavBarLink       from './NavBarLink';
 import NavBarSearchBar    from './NavBarSearchBar';
 
 class NavBarContent extends Component 
@@ -20,9 +20,9 @@ class NavBarContent extends Component
 							<NavBarDropdownItem href="/#The_Valen_Ministry" text="The Valen Ministry" />
 							<NavBarDropdownItem href="/#Dra'akna" text="Dra'akna" />
 						</NavBarDropdown>
-						<NavBarButton href="/#Special:Add_Wiki_Page" text="Add Page" active={false} />
-						<NavBarButton href="/#Special:Edit_Wiki_Page" text="Edit Page" active={false} />
-						<NavBarButton href="/#About" text="About" active={false} />
+						<NavBarLink href="/#Special:Add_Wiki_Page" text="Add Page" active={false} />
+						<NavBarLink href="/#Special:Edit_Wiki_Page" text="Edit Page" active={false} />
+						<NavBarLink href="/#About" text="About" active={false} />
 					</NavBarList>
 					<NavBarList position="right">
 						<NavBarSearchBar />
@@ -38,8 +38,8 @@ class NavBarContent extends Component
 							</NavBarDropdown>
 						) : (
 							<React.Fragment>
-								<NavBarButton href="/Sign_Up" text="Sign up" active={false} />
-								<NavBarButton href="/Sign_In" text="Sign in" active={false} />
+								<NavBarLink href="/Sign_Up" text="Sign up" active={false} />
+								<NavBarLink href="/Sign_In" text="Sign in" active={false} />
 							</React.Fragment>
 						) }
 					</NavBarList>
