639f5bb86a597530d79e45817bcec6b2792bcf74
Alex W <alexander.stephen.wall@gmail.com>
2020-11-25 22:48:26 -0500

    Expanded the functionality of Modal and the Add Webpage navbar button

--- web/frontend/src/Components/Modal.jsx
+++ web/frontend/src/Components/Modal.jsx
@@ -1,35 +1,92 @@
-import React from 'react'
+import React, { useCallback, useEffect, useRef } from 'react'
 import ReactDom from 'react-dom'
 
 const MODAL_STYLES = {
-	  position: 'fixed',
-	  top: '50%',
-	  left: '50%',
-	  transform: 'translate(-50%, -50%)',
-	  backgroundColor: '#FFF',
-	  padding: '50px',
-	  zIndex: 1000
+	position: 'fixed',
+	top: '50%',
+	left: '50%',
+	transform: 'translate(-50%, -50%)',
+	backgroundColor: '#FFF',
+	padding: '50px',
+	zIndex: 1000,
+
+	MozBorderRadius: '5px',
+	WebkitBorderRadius: '5px',
+	KhtmlBorderRadius: '5px',
 };
 
 const OVERLAY_STYLES = {
-	  position: 'fixed',
-	  top: 0,
-	  left: 0,
-	  right: 0,
-	  bottom: 0,
-	  backgroundColor: 'rgba(0, 0, 0, .7)',
-	  zIndex: 1000
+	position: 'fixed',
+	top: 0,
+	left: 0,
+	right: 0,
+	bottom: 0,
+	backgroundColor: 'rgba(0, 0, 0, .7)',
+	zIndex: 1000
 };
 
-export default function Modal({ open, children, onClose  }) {
-	if (!open)
+const X_STYLES = {
+	position: 'absolute',
+	cursor: 'pointer',
+
+	top: '-10px',
+	right: '-10px',
+
+	boxSizing: 'border-box',
+	width: '20px',
+	height: '20px',
+
+	borderWidth: '3px',
+	borderStyle: 'solid',
+	borderColor: '#605F61',
+	borderRadius: '100%',
+
+	background: '-webkit-linear-gradient(-45deg, transparent 0%, transparent 46%, white 46%,  white 56%,transparent 56%, transparent 100%), -webkit-linear-gradient(45deg, transparent 0%, transparent 46%, white 46%,  white 56%,transparent 56%, transparent 100%)',
+	backgroundColor: '#605F61'
+};
+
+export default function Modal({ isOpen, setOpen, children })
+{
+	const modalBackground = useRef(null);
+
+	const handleOutsideClick = useCallback( e =>
+		{
+			console.log(modalBackground);
+			if ( modalBackground.current.contains(e.target) )
+				setOpen(false);
+		},
+		[setOpen]
+	);
+
+	useEffect(() =>
+		{
+			if (isOpen)
+			{
+				console.log('Rendering Modal open');
+				window.addEventListener("click", handleOutsideClick);
+			}
+			else
+			{
+				console.log('Rendering Modal closed');
+				window.removeEventListener("click", handleOutsideClick);
+			}
+
+			return () => window.removeEventListener("click", handleOutsideClick);
+		},
+		[isOpen, handleOutsideClick]
+	);
+
+	if (!isOpen)
+	{
+		window.removeEventListener("click", handleOutsideClick);
 		return null;
+	}
 
 	return ReactDom.createPortal(
 		<>
-			<div style={ OVERLAY_STYLES } />
+			<div style={ OVERLAY_STYLES } ref={ modalBackground }/>
 			<div style={ MODAL_STYLES }>
-				<button onClick={ onClose }>Close Modal</button>
+				<button style={ X_STYLES } type="button" onClick={ () => setOpen(false) }></button>
 				{ children }
 			</div>
 		</>,
--- web/frontend/src/Components/NavigationBar/Admin/CreateWikiPageNavBarLink.jsx
+++ web/frontend/src/Components/NavigationBar/Admin/CreateWikiPageNavBarLink.jsx
@@ -3,18 +3,30 @@ import React, { useState } from 'react';
 import NavBarLink from '../NavBarLink';
 
 import Modal from '../../Modal';
+import TextInput from '../../Form Components/TextInput';
+import SubmitButton from '../../Form Components/SubmitButton';
+import CSRFField from '../../Form Components/CSRFField';
 
-export default function CreateWikiPageNavBarLink()
+export default function CreateWikiPageNavBarLink({ csrfHTML })
 {
-	const [isOpen, setOpen] = useState(false);
+	const [isModalOpen, setModalOpen] = useState(false);
 
 	return (
 		<>
-			<NavBarLink onClick={ () => setOpen(true) } text="Add Page" active={ false } />
-			<Modal open={ isOpen } onClose={ () => setOpen(false) }>
-				Foo bar
+			<NavBarLink
+				onClick={ () => setModalOpen(true) }
+				text="Add Page"
+				active={ false }
+			/>
+
+			<Modal isOpen={ isModalOpen } setOpen = { setModalOpen }>
+				<form action='/a/Wiki_Page' method='post' autoComplete='off'>
+					<TextInput formId='page_name' labelText='Page Name' />
+					<SubmitButton text='Create Page' />
+					<button type="button" onClick={ () => setModalOpen(false) }>Close Modal</button>
+					<CSRFField csrfHTML={ csrfHTML }/>
+				</form>
 			</Modal>
 		</>
 	);
 }
-
