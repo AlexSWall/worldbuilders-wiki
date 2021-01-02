b19661d343a60a7d1ec2b42968db4ac295c7ed0f
Alex W <alexander.stephen.wall@gmail.com>
2021-01-01 23:40:55 -0500

    Improved navigation bar SCSS, including switching to flexbox

--- web/frontend/src/Components/NavigationBar.jsx
+++ web/frontend/src/Components/NavigationBar.jsx
@@ -19,73 +19,71 @@ export default function NavigationBar()
 	const globals = useContext(GlobalsContext);
 
 	return (
-		<div id="navbarWrapper">
+		<div id="navbar-wrapper">
 			<nav id="navbar">
 				<div id="navbar-brand-wrapper">
 					<a id="navbar-brand" href="/#">
-						The Weavemajj Wiki
+						Weavemajj
 					</a>
 				</div>
-				<div id="navbar-content-wrapper">
-					<div id="navbar-content">
-						<ul className="navbar-list">
-							<Item text='Wiki Navigation' href={ false }>
-								<DropdownList>
-									<DropdownItem text='Cosmology'>
-										<DropdownList>
-											<DropdownItem text='Grand History' />
-											<DropdownItem text='Deities and Religion' />
-											<DropdownItem text='The Planes' />
-											<DropdownItem text='Magic and Natural Sciences' />
-										</DropdownList>
-									</DropdownItem>
-									<DropdownItem text='Locations'>
-										<DropdownList>
-											<DropdownItem text="Tal'Dorei" />
-											<DropdownItem text='Wildemount' />
-											<DropdownItem text='Outer Planes' />
-										</DropdownList>
-									</DropdownItem>
-									<DropdownItem text='Miscellaneous'>
-										<DropdownList>
-											<DropdownItem text='Misconceptions' />
-										</DropdownList>
-									</DropdownItem>
-									<DropdownItem text='Random Page' onClick={ () => {
-										return false;
-									} }/>
-								</DropdownList>
-							</Item>
-							{
-								globals.isAuthenticated ? (
-									<>
-										<Item text='Add Page' ModalComponent={ WikiPageCreationForm } />
-										<Item text='Edit Page' ModalComponent={ WikiPageModificationForm } />
-										<Item text='Delete Page' ModalComponent={ WikiPageDeletionForm } />
-									</>
-								) : (<React.Fragment />)
-							}
-						</ul>
-						<ul className="navbar-list navbar-list-right">
-							<SearchBar />
-							{
-								globals.isAuthenticated
-								? (
-									<Item text={ globals.preferredName || 'Account' } href={ false }>
-										<DropdownList>
-											<DropdownItem text='Change Password' ModalComponent={ ChangePasswordForm } />
-											<DropdownItem text='Sign Out' onClick={ () => signOut(globals.csrfTokens) } />
-										</DropdownList>
-									</Item>
-								) : (
-									<>
-										<Item text='Sign up' ModalComponent={ SignUpForm } />
-										<Item text='Sign in' ModalComponent={ SignInForm } />
-									</>
-								)
-							}
-						</ul>
-					</div>
+				<div id="navbar-content">
+					<ul className="navbar-list">
+						<Item text='Wiki Navigation' href={ false }>
+							<DropdownList>
+								<DropdownItem text='Cosmology'>
+									<DropdownList>
+										<DropdownItem text='Grand History' />
+										<DropdownItem text='Deities and Religion' />
+										<DropdownItem text='The Planes' />
+										<DropdownItem text='Magic and Natural Sciences' />
+									</DropdownList>
+								</DropdownItem>
+								<DropdownItem text='Locations'>
+									<DropdownList>
+										<DropdownItem text="Tal'Dorei" />
+										<DropdownItem text='Wildemount' />
+										<DropdownItem text='Outer Planes' />
+									</DropdownList>
+								</DropdownItem>
+								<DropdownItem text='Miscellaneous'>
+									<DropdownList>
+										<DropdownItem text='Misconceptions' />
+									</DropdownList>
+								</DropdownItem>
+								<DropdownItem text='Random Page' onClick={ () => {
+									return false;
+								} }/>
+							</DropdownList>
+						</Item>
+						{
+							globals.isAuthenticated ? (
+								<>
+									<Item text='Add Page' ModalComponent={ WikiPageCreationForm } />
+									<Item text='Edit Page' ModalComponent={ WikiPageModificationForm } />
+									<Item text='Delete Page' ModalComponent={ WikiPageDeletionForm } />
+								</>
+							) : (<React.Fragment />)
+						}
+					</ul>
+					<ul className="navbar-list navbar-list-right">
+						<SearchBar />
+						{
+							globals.isAuthenticated
+							? (
+								<Item text={ globals.preferredName || 'Account' } href={ false }>
+									<DropdownList>
+										<DropdownItem text='Change Password' ModalComponent={ ChangePasswordForm } />
+										<DropdownItem text='Sign Out' onClick={ () => signOut(globals.csrfTokens) } />
+									</DropdownList>
+								</Item>
+							) : (
+								<>
+									<Item text='Sign up' ModalComponent={ SignUpForm } />
+									<Item text='Sign in' ModalComponent={ SignInForm } />
+								</>
+							)
+						}
+					</ul>
 				</div>
 			</nav>
 		</div>
