13ef6f4e5cd8040ba888be0609a725ca9e9a69ed
Alex W <alexander.stephen.wall@gmail.com>
2021-10-03 03:26:19 +0100

    Removing trailing whitespace from bootstrapper.php

--- web/backend/bootstrap/bootstrapper.php
+++ web/backend/bootstrap/bootstrapper.php
@@ -239,7 +239,7 @@ $logger->info('Adding middleware.');
 	 * @param bool                  $displayErrorDetails -> Should be set to false in production
 	 * @param bool                  $logErrors -> Parameter is passed to the default ErrorHandler
 	 * @param bool                  $logErrorDetails -> Display error details in error log
-	 * @param LoggerInterface|null  $logger -> Optional PSR-3 Logger  
+	 * @param LoggerInterface|null  $logger -> Optional PSR-3 Logger
 	 *
 	 * Note: This middleware should be added last. It will not handle any exceptions/errors
 	 * for middleware added after it.
