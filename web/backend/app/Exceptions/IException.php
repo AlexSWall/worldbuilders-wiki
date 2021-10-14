2ccda4f7201c1e659a9b15e5e33b040eca5d2c9c
Alex W <alexander.stephen.wall@gmail.com>
2021-10-14 11:52:19 +0100

    Added CustomException, IException, and ServerException to backend code

---
+++ web/backend/app/Exceptions/IException.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Exceptions;
+
+interface IException
+{
+	/* Protected methods inherited from Exception class */
+	public function getMessage();                 // Exception message
+	public function getCode();                    // User-defined Exception code
+	public function getFile();                    // Source filename
+	public function getLine();                    // Source line
+	public function getTrace();                   // An array of the backtrace()
+	public function getTraceAsString();           // Formated string of trace
+
+	/* Overrideable methods inherited from Exception class */
+	public function __toString();                 // formatted string for display
+	public function __construct($message = null, $code = 0);
+}
